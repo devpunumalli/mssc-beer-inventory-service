183d61ab0bfc8cee8a5f2bc484e7e3049fa507a0
devpunumalli <devpunumalli@gmail.com>
2020-12-31 12:14:58 -0500

    Add DB Init scripts,closes #3

--- src/main/java/com/dev/msscbeerinventoryservice/MsscbeerinventoryserviceApplication.java
+++ src/main/java/com/dev/msscbeerinventoryservice/MsscbeerinventoryserviceApplication.java
@@ -1,9 +1,11 @@
 package com.dev.msscbeerinventoryservice;
 
+import org.jgroups.blocks.locking.AwaitInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
 public class MsscbeerinventoryserviceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MsscbeerinventoryserviceApplication.class, args);
