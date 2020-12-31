b475142386b87a1bec93e91a8dd03f8781b515ef
devpunumalli <devpunumalli@gmail.com>
2020-12-31 17:04:18 -0500

    configuring artemis MQ server,closes #5

--- src/main/java/com/dev/msscbeerinventoryservice/MsscbeerinventoryserviceApplication.java
+++ src/main/java/com/dev/msscbeerinventoryservice/MsscbeerinventoryserviceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
-@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
+@SpringBootApplication
 public class MsscbeerinventoryserviceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MsscbeerinventoryserviceApplication.class, args);
